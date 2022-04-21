97415ab515a1734144d365ff75ed1afecaa857b2
Evan Liapakis <awesomethngs@gmail.com>
2022-04-21 17:59:10 +1000

    Basic proof of concept of what getting routes looks like

--- greenpath/src/App.js
+++ greenpath/src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+const sampleApiReturn = [
+  {
+    method: "bike",
+    footprint: 0,
+    rank: 0,
+  },
+  {
+    method: "car",
+    footprint: 100,
+    rank: 2,
+  },
+  {
+    method: "walk",
+    footprint: 0,
+    rank: 1,
+  }
+];
+
+function RouteGetter(props) {
+  return (
+    <button onClick={props.onClick}>Get Routes</button>
+  );
+}
+
+function Route(props) {
+  if (props.route) {
+    return (
+      <div>
+        <h3>Method: {props.route.method}</h3>
+        <p>Footprint: {props.route.footprint}</p>
+      </div>
+    );
+  }
+}
+
+class RouteRenderer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      routes: Array(sampleApiReturn.length).fill(null),
+    };
+  }
+
+  getAllRoutes() {
+    const routes = this.state.routes.slice();
+    sampleApiReturn.forEach(route => routes[route.rank] = route);
+    this.setState({ routes: routes });
+    console.log(routes);
+  }
+
+  renderRoute(route) {
+    return <Route route={route} />;
+  }
+
+  renderAllRoutes() {
+    const routes = [];
+    this.state.routes.forEach(route => routes.push(this.renderRoute(route)));
+    return routes;
+  }
+
+  renderRouteGetter() {
+    return <RouteGetter onClick={() => this.getAllRoutes()} />;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.renderAllRoutes()}
+        {this.renderRouteGetter()}
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouteRenderer />
     </div>
   );
 }
