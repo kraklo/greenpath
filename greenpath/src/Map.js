2182b3ba34803d7daef83626b4bc51d1dd8de016
Alexander Kam <kamzhanyue@gmail.com>
2022-04-22 16:08:52 +1000

    Refactor Maps

--- greenpath/src/Map.js
+++ greenpath/src/Map.js
@@ -1,12 +1,42 @@
-import React from "react";
-import MapContainer from "./MapContainer.js";
+import React, {Component} from 'react';
+import {DirectionsRenderer, GoogleMap, LoadScript} from "@react-google-maps/api";
 
-function Map() {
-    return (
-        <div style={{ display: "flex", height: "calc(100vh - 50px)" }}>
-            <MapContainer />
-        </div>
-    );
+const GOOGLE_API_KEY = 'AIzaSyCHWC6UAUW6eR6CGZjHnnCFkBrczCClb6Q';
+const libraries = ["places"];
+
+const mapContainerStyle = {
+    width: '100vw',
+    height: '100vh',
+    position: 'absolute',
+    top: '67px'
+};
+
+const center = {
+    lat: -37.796368,
+    lng: 144.961166
+};
+
+class Map extends Component {
+    render() {
+        return (
+            <LoadScript
+                id="script-loader"
+                googleMapsApiKey={GOOGLE_API_KEY}
+                language="en"
+                region="EN"
+                version="weekly"
+                libraries={libraries}
+            >
+                <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={10}>
+                    {/*<DirectionsRenderer*/}
+                </GoogleMap>
+            </LoadScript>
+        )
+    }
 }
 
-export default Map;
+export default function GetMap() {
+    return (
+        <Map/>
+    )
+}
