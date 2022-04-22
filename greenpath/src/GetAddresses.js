ebe752bbcd7da13424a6cf17ca9b6ab7c926c1ce
Evan Liapakis <awesomethngs@gmail.com>
2022-04-22 14:32:49 +1000

    everything is in react now

--- greenpath/src/GetAddresses.js
+++ greenpath/src/GetAddresses.js
@@ -1,27 +1,78 @@
 import React from 'react';
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import Autocomplete from 'react-google-autocomplete';
 import './GetAddresses.css';
 
 const API_KEY = 'AIzaSyAehgtggYLjbAZ8WQQ4hcOYq1MkbyTGKGU';
 
-const Component = () => (
-    <div>
-        <GooglePlacesAutocomplete
+const options = {
+    fields: ['formatted_address'],
+    componentRestrictions: { 'country': ['AU'] },
+    types: []
+};
+
+// function getAddresses() {
+//     return {
+//         origin: autocompleteOrigin.autocomplete.getPlace()?.formatted_address,
+//         destination: autocompleteDestination.autocomplete.getPlace()?.formatted_address
+//     }
+// }
+
+function AutocompleteField(props) {
+    return (
+        <Autocomplete
             apiKey={API_KEY}
+            onPlaceSelected={(place) => props.addressSelected(place)}
+            options={options}
         />
-    </div>
-);
+    );
+}
+
+class GetAutocompletes extends React.Component {
+    constructor(props) {
+        super(props);
+        this.setState({
+            originAddress: '',
+            destinationAddress: ''
+        });
+    }
+
+    originAddressSelected(place) {
+        const addressOrigin = place?.formatted_address;
+
+        if (!addressOrigin) {
+            // find a way to change placeholder
+        } else {
+            this.setState({ originAddress: addressOrigin });
+        }
+    }
+
+    destinationAddressSelected(place) {
+        const addressDestination = place?.formatted_address;
+
+        if (!addressDestination) {
+            // find a way to change placeholder
+        } else {
+            this.setState({ destinationAddress: addressDestination });
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <div className='input_wrapper'>
+                    <AutocompleteField addressSelected={(place) => this.originAddressSelected(place)} />
+                </div>
+                <div className='input_wrapper'>
+                    <AutocompleteField addressSelected={(place) => this.destinationAddressSelected(place)} />
+                </div>
+            </>
+        );
+    }
+}
 
 function GetAddresses() {
     return (
-        <div>
-            <div className='input_wrapper'>
-                <input id="autocomplete_origin"></input>
-            </div>
-            <div className='input_wrapper'>
-                <input id="autocomplete_destination"></input>
-            </div>
-        </div>
+        <GetAutocompletes />
     );
 }
 
